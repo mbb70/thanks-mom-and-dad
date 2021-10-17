8c0dac57e92218ccdf18ce530f2f13bd76711dde
Matthew Brown <matt@mbb.dev>
2021-10-17 18:15:57 -0400

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import sjcl from './features/thanks/sjcl';
-import { message } from './features/thanks/message';
+import { message, ps } from './features/thanks/message';
 
-function attemptDecrypt(phone: String, car: String, message: String, setDisplay: Function, setRevealedIndex: Function) {
+function attemptDecrypt(phone: string, car: string, message: string, setDisplay: Function, encryptedPS: string, setPS: Function, setRevealedIndex: Function) {
   const inputs = [phone, car].map((s) => s.toLowerCase().replace(/[ \-_()]/g, ''));
   const password = inputs.join('-')
   try {
     const display = (sjcl as any).decrypt(password, message);
+    const ps = (sjcl as any).decrypt(password, encryptedPS);
     setDisplay(display);
+    setPS(ps);
     incrementallyReveal(setRevealedIndex, 0, display.length);
   } catch {
   }
@@ -22,12 +24,14 @@ function incrementallyReveal(setRevealedIndex: Function, index: number, max: num
   }, 0.01)
 }
 
+const encrypted = JSON.parse(message).ct;
+
 function App() {
   const [phone, setPhone] = React.useState('');
   const [car, setCar] = React.useState('');
   const [display, setDisplay] = React.useState('');
+  const [plainPS, setPS] = React.useState('');
   const [revealedIndex, setRevealedIndex] = React.useState(0);
-  const encrypted = JSON.parse(message).ct;
   const displayText = (revealedIndex === 0 || revealedIndex < display.length) ? display.slice(0, revealedIndex) + encrypted.slice(revealedIndex) : display;
   return (
     <div className={`sm:mt-8 grid justify-items-center`}>
@@ -47,7 +51,7 @@ function App() {
               <input className="border rounded shadow text-xl" value={car} onChange={(e) => setCar(e.target.value)}/>
             </div>
             <div>
-            <button type="button" onClick={() => attemptDecrypt(phone, car, message, setDisplay, setRevealedIndex) } className="bg-blue-300 rounded shadow text-xl p-2">
+            <button type="button" onClick={() => attemptDecrypt(phone, car, message, setDisplay, ps, setPS, setRevealedIndex) } className="bg-blue-300 rounded shadow text-xl p-2">
               Decrypt
             </button>
             </div>
@@ -55,6 +59,9 @@ function App() {
         </form>
         <div className={`mt-4 ${displayText === display ? '' : 'break-all' }`}>
          <div dangerouslySetInnerHTML={{__html: displayText}}></div>
+        </div>
+        <div style={{display: displayText === display ? '' : 'none'}}>
+          <div dangerouslySetInnerHTML={{__html: plainPS}}></div>
         </div>
       </div>
     </div>
